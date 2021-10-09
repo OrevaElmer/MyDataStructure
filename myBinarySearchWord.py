0bc26b1f68fb9351d80ed8eac29540d7db65c821
Egalase Oreva Elmer <egalaseorevaelmer@gmail.com>
2021-10-09 13:07:23 +0100

    Update myBinarySearchWord.py

--- myBinarySearchWord.py
+++ myBinarySearchWord.py
@@ -1,7 +1,7 @@
 #This program search a list of names from another list of names:
 
 import sys
-#from treeHouse.load import load_strings
+
 from loadNumberString import load_strings
 
 fullname = load_strings(sys.argv[1])
@@ -26,9 +26,9 @@ for name in searchNames:
     indexOfnames = binarySearch(fullname, name)
     print(indexOfnames)
 
-#print(linearSearch(fullname,'Felicity Davies'))
+
 #This is how I called the sorted text or unsorted text:
-# c:/xampp/htdocs/pythonLesson/myAlgorithm/myBinaryLinear.py names/unsorted.txt
+# c:/xampp/htdocs/pythonLesson/myAlgorithm/myBinarySearchWord.py names/unsorted.txt
 
     
     
